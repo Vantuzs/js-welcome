4470bec9c1121f67e516d4d05009a0b8a27790da
Vantuz <Shcherbina444@gmail.com>
2024-10-15 15:28:35 +0300

    Erorr

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ box2 = 15;
 const secretWord = 'superSecret!Kyivstar!Cybersecuruti';
 console.log(secretWord);
 
-// secretWord = 'wordd'; <<-- перевизначить константу невозможно!!! 0о0
+secretWord = 'wordd'; // <<-- перевизначить константу невозможно!!! 0о0
+
+console.log(box2);
