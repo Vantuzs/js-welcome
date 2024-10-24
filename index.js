0d0f3f2f40bd6c12c144d9373b3db35201058dc6
Vantuz <Shcherbina444@gmail.com>
2024-10-24 18:42:18 +0300

    High Order Function

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-function sum(a,b){
-  return a+b;
+/*
+
+High Order Function (HOF, Функция высшего порядка) - 
+функция, которя принимает другою функцию в качесве аргумента
+
+
+*/
+
+function calculator(num1, num2, functionLink){ // HOF
+  let result = functionLink(num1,num2);
+  return result;
+} 
+                      //num1    //num2      //functionLink
+console.log( calculator(5,        5,          sum)); // sum - посылание на функцию
+
+// Используя калькулятор, поднесите число 1 в степень числа 2
+console.log( calculator(3, 2, function (a, b) { // callback
+  return a**b;
+}));
+
+
+function sum(a, b) {
+  return a + b;
 }
 
-let superFunction = sum; // насправди, sum - посилання на ячейку в RAM
+function substract(a, b) {
+  return a - b;
+}
 
-console.log( sum(2 ,4)); // 0X00045
+function multy(a, b) {
+  return a * b;
+}
 
-console.log(superFunction(5, 5)); // 0X00045
+function divide(a, b) {
+  return a / b;
+}
+/*
+
+High Order Function(HOF, Функция высшего порядка) - 
+функция, которя принимает другою функцию в качесве аргумента 
+
+Callback - Колбек (Функция зворотньго виклику),
+функция, ЯКУ мы передали как агрумент при вызове другой фукнции
+
+ */
