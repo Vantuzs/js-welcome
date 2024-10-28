f536d18ff6f0160b4713e7d5b70140baf631cd55
Vantuz <Shcherbina444@gmail.com>
2024-10-28 16:48:06 +0200

    Constructow welcome

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const cat = {
-    name: 'Kuper',
-    age: '9',
-    color: 'Orange-white',
-    running: function(){
-        return 'I Nerealno begu';
-    },
-    sleep: function(){
-        return 'I am sleepeng, Dont tuch me=3';
-    },
-    meow: function(){
-        return 'Meooooooooooooooooooow WaaaaaaaaaaaaaauuuuuuuuuWwwwwwwww';
+/* Способи створення об'єкта
+
+const cat = { // літеральне створення об'єкта
+
+}
+
+const cat2 = Object(); // Функція-конструктор
+
+const cat3 = new Object(); // Функція-конструктор з оператором new
+
+*/
+
+// Конструктор визначае шаблот або "скелет" Обьектов, которые будут создаваться
+
+/*
+Угоды у фукнциях конструкторах:
+
+1. Имя фукнции конструктора мае починатыся с большой буквы
+2. Фукнция конструктор должна выполняться только за помощью оператора new
+
+
+*/
+
+// Задача: написать функцию конструктор для создания кота
+
+function Cat(name, color, breed, age, speed){
+    this.name = name; // this - посилання на ось цього новостворенного кота
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+    this.run = function(){
+        return this.name + ' is runing ' + speed + ' km';
     }
 }
 
-cat['jump height'] = '2m';
-cat.timeSleep = '+- 1h';
-cat.runningSpeed = function(){
-    return "I am FLESHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH";
-}
+// Задача: создать несколько (двух) котов, с помощью нашей фукнции конструктора
+
+const cat1 = new Cat('Bublik','white','Dvorovyi', 3, 15);
+const cat2 = new Cat('Murzik', 'black', 'siam', 4, 12);
