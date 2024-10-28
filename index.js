81b0c5e5f62f1d8f84e5fd3a810daedebdd35248
Vantuz <Shcherbina444@gmail.com>
2024-10-28 16:20:32 +0200

    Practic task resolve

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-/* Способы створення обьекта
-
-const cat = { // литеральное создание обьекта(литеральный - буквальный, дословный)
-
-}
-
-const cat2 = Object(); // Функция конструктор
-
-const cat3 = new Object(); // Функция конструктор с оператором new
-
-// Синтаксический цукор - спрощення синтаксису мови програмування что бы человеку было проще работать(Они не добовляют новые возможности)
-
-*/
-
 const cat = {
-    name: 'Murzik',
-    color: 'Red',
-    breed: 'Dvorovyi',
-    age: 2,
-    eat: function(){
-        return 'I am eating';
+    name: 'Kuper',
+    age: '9',
+    color: 'Orange-white',
+    running: function(){
+        return 'I Nerealno begu';
     },
     sleep: function(){
-        return 'I am  sleeping';
+        return 'I am sleepeng, Dont tuch me=3';
+    },
+    meow: function(){
+        return 'Meooooooooooooooooooow WaaaaaaaaaaaaaauuuuuuuuuWwwwwwwww';
     }
 }
 
-console.log(cat.age); // age = 2
-
-cat.age = cat.age + 1; // age =3
-console.log(cat.age); // age =3
-
-cat.age++; // age = 4
-
-console.log(cat.age); // age = 4
-
-cat.sleep = undefined; // Bad practice
-
-delete cat.sleep; // Good practice
-
-// Adding properties
-
-// Задача: Додати имя друга Мурзика до обьекта Мурзика
-
-cat.friend = 'Tuzik';
-
-cat['favorite food'] = 'fish';
-
-cat.meow = function(){
-    return 'Meow';
+cat['jump height'] = '2m';
+cat.timeSleep = '+- 1h';
+cat.runningSpeed = function(){
+    return "I am FLESHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH";
 }
