5025a3beb773e41977be2164161bf01288378868
Vantuz <Shcherbina444@gmail.com>
2024-10-28 21:10:37 +0200

    Practise task resolve AUTO

--- index.js
+++ index.js
@@ -1,86 +1,72 @@
-/* Разработка програмы для управления книжным магазом
+/*
 
-Предметная область: В книжном магазе у нас будет много книг. Мы их продаём.
+Задача 1
 
-Все наши книги по сути можно представить в виде объекта с одинаковой структурой.
-
-done -->> Задача: создать констуктор объектов для представления книг с следующими властивостями.
-Название книги, автор, год создания, цена
-
-Спроэктировать методы:
-Спроэктировать методы для получения и установления значений этих властивостей
-
-done -->> Задача 2: Сделать для всех книг метод, который бы повернул цену книги, але с скидкой
+Сделать функцию конструктор Country, которая принимает в качестве аргументов
+- название страны
+- популяцию (population)
+- площа (area)
+Спроэктировать метод густоты населения
+густота населення = популяция / площадь
 */
 
-function Book(title, author, year, price){
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.price = price;
-
-    this.getTitle = function(){
-        // мы можем реализовать, проверку права на доступ до властивости
-
-        return this.title;
-    }
-
-    this.getAuthor = function(){
-        // мы можем реализовать, проверку права на доступ до властивости
-
-        return this.author;
-    }
-
-    this.getYear = function(){
-        // мы можем реализовать, проверку права на доступ до властивости
-        return this.year;
-    }
-
-    this.getPrice = function(){
-        // мы можем реализовать, проверку права на доступ до властивости
-
-        return this.price;
-    }
-
-    this.setTitle = function(newTitle){
-        // Могли реализовать, напривер, какието проверки - например на право(может не может)
-        this.title = newTitle;
-    }
-
-    this.steAuthor = function(newAuthor){
-        // Могли реализовать, напривер, какието проверки - например на право(может не может)
-
-        this.author = newAuthor;
-    }
-
-    this.setYear = function(newYear){
-        // Могли реализовать, напривер, какието проверки - например на право(может не может)
-
-        this.year = newYear;
-    }
-
-    this.setPrice = function(newPrice){
-        // Могли реализовать, напривер, какието проверки - например на право(может не может)
-
-        this.price = newPrice;
-    }
-
-    this.calcaleteDiscountPrice = function(discountPercentage){
-        const discountedPrice = this.price - (this.price*(discountPercentage/100));
-        return discountedPrice;
+function Country(name,population,area){
+    this.name = name;
+    this.population = population;
+    this.area = area;
+    this.gustota = function(){
+        let gustota;
+        gustota = this.population/area;
+        return gustota;
     }
 }
 
-const book1 = new Book('Чотири витры', 'Кристин Генна', 2021, 450.00);
+const coutry1 = new Country('Ukraine',41130432, 603628 );
+const coutry2 = new Country('Germany',84270625, 357,592);
 
-// console.log( book1.getTitle());
-// console.log( book1.getAuthor());
-// console.log( book1.getYear());
-// console.log( book1.getPrice());
+/*
 
-// book1.setTitle('New Title');
-// book1.steAuthor('New Author');
-// book1.setYear(2025);
-// book1.setPrice(350);
+Задача 2:
 
-console.log(book1.calcaleteDiscountPrice(50));
+Сделать функцию-конструктор Авто, которое имеет:
+- название
+- максимальная скорость
+- поточная скороть
+Спроэктировать методы:
+- метод прискорення (accelearte) - принимает в качестве аргумента певне прискорення
+Зауважте, прискорення !== >макс скорости
+- метод торможение-снижение скорости (deacclerate) -принимает в качестве аргумента певне сповильнення
+Зауважте, що поточна скорость авто не может быть <0
+- метод остановки (stop)
+
+*/
+
+function Auto(name,maxspeed,potocSpeed,){
+    this.name = name;
+    this.maxspeed = maxspeed;
+    this.potocSpeed = potocSpeed;
+    this.accelearte = function(speedUp){
+        speedUp=this.potocSpeed+speedUp;
+        if(speedUp>this.maxspeed){
+            return 'Erorr';
+        } else{
+            this.potocSpeed = speedUp;
+            return this.potocSpeed;
+        }
+    }
+    this.deaccelearte = function(speedDown){
+        speedDown = this.potocSpeed-speedDown;
+        if(speedDown<0){
+            return 'Erorr';
+        } else {
+            this.potocSpeed = speedDown;
+            return this.potocSpeed
+        }
+    }
+    this.stop = function(){
+        
+        return this.potocSpeed-this.potocSpeed;
+    }
+}
+
+const car1 = new Auto('Lanus', 400,100);
