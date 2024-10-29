f0af6bdb2643736ad06a9d2a82b29d5b1d916c40
Vantuz <Shcherbina444@gmail.com>
2024-10-29 19:48:13 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ const greets = {
     30: 'Good mornig!'
 }
 
-const valueFormUser = 19;
+// const valueFormUser = 19;
 
-console.log(greets[valueFormUser]);
+// console.log(greets[valueFormUser]);
 // greets[19]
+
+/*
+
+Задача: у наст есть обьект, ключем в котором есть день недели, а значением планы на этот день
+Принять от пользователя через prompt, названия дня недели и выдать алерт,
+ что запланировано у пользователя на этот день
+
+*/
+
+const obj = {
+    Mon: 'drive lesson',
+    Tue: 'Dantist visit',
+    Wed: 'go to patry',
+    Thu: 'hard work',
+    Fri: 'chill',
+    Sat: 'go to park'
+}
+
+const a = prompt('Введите день недели \nMon\nTue\nWed\nThu\nFri\nSat ');
+
+alert(obj[a]? obj[a] : 'Error happend')
+
