0662e228c8c2158f30052d7dbf399ac3162f877e
Vantuz <Shcherbina444@gmail.com>
2024-10-29 19:53:13 +0200

    Operator in

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-/*
 
-Задача: у нас есть обьект с разными версиями приветствий для пользователя.
-
-*/
-
-const greets = {
-    15: 'Hi',
-    17: 'Ya, whats up',
-    19: 'Hello',
-    30: 'Good mornig!'
-}
-
-// const valueFormUser = 19;
-
-// console.log(greets[valueFormUser]);
-// greets[19]
-
-/*
-
-Задача: у наст есть обьект, ключем в котором есть день недели, а значением планы на этот день
-Принять от пользователя через prompt, названия дня недели и выдать алерт,
- что запланировано у пользователя на этот день
-
-*/
 
 const obj = {
     Mon: 'drive lesson',
@@ -30,10 +6,13 @@ const obj = {
     Wed: 'go to patry',
     Thu: 'hard work',
     Fri: 'chill',
-    Sat: 'go to park'
+    Sat: 'go to park',
+    'favorite color': 'white',
+    0: 'value'
 }
 
-const a = prompt('Введите день недели \nMon\nTue\nWed\nThu\nFri\nSat ');
+// оператор in 
+// in - бинарный оператор
+// "key" in obj
 
-alert(obj[a]? obj[a] : 'Error happend')
-
+console.log( 'name' in obj);
