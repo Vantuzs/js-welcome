c60741daee8d0589ab5b4078371bd44123ba5f73
Vantuz <Shcherbina444@gmail.com>
2024-10-29 20:10:55 +0200

    Loop: for-in

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function greeting(user){
-    return `Hello ${'name' in user ? user.name : 'Anonym'}`;
+const user = {
+    name: 'John',
+    lastname: 'Doe',
+    age: 20,
+    'favorite color': 'red',
+    movie: 'Blue moon',
+    avatar: 'http://.....'
 }
 
-const user = {
-    email: 'john.doe@gmail.com',
-    name: 'Peter'
+// for..in  -используется для перебора (Итерации) ключей в обьекте
+// НЕ ЭТОТ for(;;)
+/*
+
+for (variable in object){
+    // Код что выполняется для каждой властивости
+}
+
+variable - переменная, которая будет принимать значение ключей властивостей
+object - обьект, властивости (ключи) которого мы хотим перебрать
+
+*/
+
+// Задача: в обьекте user вывести название всех ключей и властивости
+
+for(let key in user){
+    console.log(`${key} ---> ${user[key]}`); // key ---> value
 }
