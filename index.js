ae55253e1fb397454a29f15dc00b599db8c14f55
Vantuz <Shcherbina444@gmail.com>
2024-10-29 19:31:21 +0200

    Another example of how computing properties work

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
+/*
 
-const user = {
-    name: 'John',  // ключ 'name', значення - 'John'
-    'favorite color': 'red',
-    0: 'value',
+Задача: у нас есть обьект с разными версиями приветствий для пользователя.
+
+*/
+
+const greets = {
+    15: 'Hi',
+    17: 'Ya, whats up',
+    19: 'Hello',
+    30: 'Good mornig!'
 }
 
-// Ключом в обьекте может быть рядок или тип данных Symvol
+const valueFormUser = 19;
 
-// . - оператор доступа за именем властивости (когда речь идет про валидные индефикаторы)
-
-console.log(user.name); // John
-// user.'faborite color'; -->> syntax error
-// user.0; -->> syntax error
-
-
-// [] - Оператор доступа до обчислювальнои властивости (когда речь идет про невалидные индефикаторы)
-
-console.log(user['favorite color']); // red
-console.log(user[0]); // value
-
-const someObj = {
-    2: 10
-}
-
-// Задача: получить значение, которое находиться под клбчем 2
-
-console.log(someObj[2]);
-console.log(someObj['2']);
+console.log(greets[valueFormUser]);
+// greets[19]
