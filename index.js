89e0aa7d6a9b861fc0dd55420083fbaa8a425e88
Vantuz <Shcherbina444@gmail.com>
2024-10-30 12:40:28 +0200

    Home practise resolve

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-const user = {
-    name: 'John',
-    lastname: 'Doe',
-    age: 20,
-    'favorite color': 'red',
-    movie: 'Blue moon',
-    avatar: 'http://.....'
-}
-
-// for..in  -используется для перебора (Итерации) ключей в обьекте
-// НЕ ЭТОТ for(;;)
 /*
 
-for (variable in object){
-    // Код что выполняется для каждой властивости
-}
-
-variable - переменная, которая будет принимать значение ключей властивостей
-object - обьект, властивости (ключи) которого мы хотим перебрать
+Задача: у нас есть обьект с зарплатами по всем отделам
+Написать функцию, которая возвращает загальну сумму, вытрачену на зарплату всех отделах
 
 */
 
-// Задача: в обьекте user вывести название всех ключей и властивости
 
-for(let key in user){
-    console.log(`${key} ---> ${user[key]}`); // key ---> value
+
+const departmentSalary = {
+    HR: 120000,
+    development: 5500000,
+    PR: 50000,
+    marketing: 120000,
+    assistent: undefined
+
 }
 
-/*
+function pereschetAllZarplata(obj){
+    let result=null;
+    for(let key in obj){
+        if(typeof obj[key] === 'undefined'){
+            console.log(`У этого отдела неизвестная зарплата ${key}: ${obj[key]}`);
+        } else if(typeof obj[key] === 'number'){
 
-Задача: написать фукнцию, которая принимает обьект и выводит все значения всех властивостей, оформленни *...*
-а ключи = "..."
-*/
-
-const obj = {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-    key4: 'value4',
-    key5: 'value5',
-    
-}
-
-function perebor(obect){
-    for(let key in obect){
-        console.log(`"${key}" ---> *${obect[key]}*`);
+            result += obj[key];
+        }
     }
+    console.log(`Общая сумма затраты на зарплату = ${result}`);
+    return;
 }
 
-perebor(obj);
-perebor(user);
+
+ pereschetAllZarplata(departmentSalary);
