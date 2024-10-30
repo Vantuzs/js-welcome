9869e4e6ff8995b81c79c437c1ad752181939e31
Vantuz <Shcherbina444@gmail.com>
2024-10-30 22:11:52 +0200

    12.JS. Objects

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
-// array.concat - используется для обьеднання двох або больше масивов в новый масив
-// array.concat(array1,array2, ..., arrayN)
+//1
+const user = {
+    name: 'John',
+    lastName: 'Doe',
+    email: 'JohnDoe01@gmail.com',
+    password: 'qwe123',
+    number: +380663223220,
+    adres: {
+        city: 'Bubrovka',
+        uliza: 'Snorovskaya',
+        dom: '32c',
+        kv: 322
+    }
 
-const array1= [1,2,2,4];
-const array2 = array1.concat([222,888,333],[55555,7777777])
-console.log(array2);
+}
+//2
+function hello(obj){
+    console.log(`Hello mister ${obj.name} ${obj.lastName}!`);
+}
 
-// array.fill() - используется для заполнения всех элементов масива какмито одним значением
-// array.fill(value,[start,[end]]); // start, end - не обязательные
+hello(user);
 
-const array3 = new Array(5);
-array3.fill(1);
+//3
 
-// array.includes - используется для проверки наявности определённого значения в масиве
-// array.includes(searchValue, [fromIndex])
+function outputOfTheEntireObject(obj){
+for(let key in obj){
+    console.log(obj[key]);
+}
+}
 
-const array4 = [2,12,1,5,8,13,12];
-console.log( array4.includes(2));// true
+outputOfTheEntireObject(user);
 
-// array.indexOf - используется для получения первого вхождения определённого значения в масиве
-// array.undexOf(serchElement, [fromIndex])
-console.log( array4.indexOf(12));
-console.log( array4.indexOf(12,-1));
+//4-5
 
+function Auto(name,maxspeed,potocSpeed,){
+    this.name = name;
+    this.maxspeed = maxspeed;
+    this.potocSpeed = potocSpeed;
+    this.accelearte = function(speedUp){
+        speedUp=this.potocSpeed+speedUp;
+        if(speedUp>this.maxspeed){
+            return 'Erorr';
+        } else{
+            this.potocSpeed = speedUp;
+            return this.potocSpeed;
+        }
+    }
+    this.deaccelearte = function(speedDown){
+        speedDown = this.potocSpeed-speedDown;
+        if(speedDown<0){
+            return 'Erorr';
+        } else {
+            this.potocSpeed = speedDown;
+            return this.potocSpeed;
+        }
+    }
+    this.stop = function(){
+        
+        return this.potocSpeed-this.potocSpeed;
+    }
+}
 
+const car1 = new Auto('Lanus', 330,100);
 
-// array.join - используется для создания рядка, обьединяя все элементы масива
-// array.join([separator])
-
-const array5 = ['Hello', 'world', 4,2,'hi'];
-console.log(array5.join('/'));
-
-
-// array.push - используется для додавання одного или нескольки елементов в конец масива
-// array.push(elemetn1, ... , elementN);
-
-const array6 = [2,3,4,5];
-array6.push(6,7,8,9,10);
-console.log(array6);
-
-
-// array.pop - он вызывается на масиве и не принимает никаких аргуметнов, Используется для удаления последнего элемента
-array6.pop();
-
+const car2 = new Auto('Tavria',300, 100);
