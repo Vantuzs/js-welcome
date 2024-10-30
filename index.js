39f55af92d679cb0f11f1b9dad92f91a5603f7f4
Vantuz <Shcherbina444@gmail.com>
2024-10-30 13:16:56 +0200

    Array welcome

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-/*
+// У наст есть какойто юзер
 
-Задача: у нас есть обьект с зарплатами по всем отделам
-Написать функцию, которая возвращает загальну сумму, вытрачену на зарплату всех отделах
+const userTelephone1 = '380661233232';
+const userTelephone2 = '380664442233';
+const userTelephone3 = '380661233255';
 
-*/
-
-
-
-const departmentSalary = {
-    HR: 120000,
-    development: 5500000,
-    PR: 50000,
-    marketing: 120000,
-    assistent: undefined
-
+const userTelephones = {
+    0: '380661233232',
+    1: '380664442233',
+    2: '380661233255'
 }
 
-function pereschetAllZarplata(obj){
-    let result=null;
-    for(let key in obj){
-        if(typeof obj[key] === 'undefined'){
-            console.log(`У этого отдела неизвестная зарплата ${key}: ${obj[key]}`);
-        } else if(typeof obj[key] === 'number'){
+// Масив (Array)
 
-            result += obj[key];
-        }
+// Оголошення масиву
+
+// 1 способ
+const arr = [2, 4, 6, 7, 5];
+
+// 2 способ
+const arr2 = new Array(2, 4, 6, 7);
+
+// Способы доступа до элементов масива
+arr[0]; // Считывание значений элементов масива
+arr[0] = 200; // Присвоение новых значений для елементов масива
+
+// +Задача: вывсети все значения масива в консоле
+
+// вариан 1
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+// вариант 2
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// Задача: у нас есть масив чисел. Вывести на консоль только парные числа
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbersArray.length;i++){
+    if(numbersArray[i]%2===0){
+        console.log(numbersArray[i]);
     }
-    console.log(`Общая сумма затраты на зарплату = ${result}`);
-    return;
 }
-
-
- pereschetAllZarplata(departmentSalary);
