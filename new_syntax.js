36b8d81e37421831b84d83304ed6000b80c39bec
Vantuz <Shcherbina444@gmail.com>
2024-11-16 12:33:58 +0200

    RangeValidator task resolve

--- new_syntax.js
+++ new_syntax.js
@@ -12,7 +12,7 @@ class Test{
     // Вариант 2 для оголошення статик метода - более популярный
     static myStaticMethod(parameter){
         // тело функции - чтото делаем 
-        console.log(parameter);
+        // console.log(parameter);
     }
 }
 
@@ -30,3 +30,52 @@ class Test{
 
 Test.myStaticMethod('123');
 const obj1 = new Test('test1','test2'); // создание нового екземпляра класа Test  
+
+
+class RangeValidator{
+    constructor(from,to){
+        this.to = to;
+        this.from = from;
+    }
+
+
+    set from(newValue){
+        if(typeof newValue !== 'number'){
+            throw new TypeError('From must be a number')
+        }
+        if(newValue> this._to){
+            throw new RangeError('From must be a not > to ')
+        }
+        this._from = newValue
+    }
+
+    get from(){
+        return this._from
+    }
+
+    set to(newValue){
+
+        if(typeof newValue !== 'number'){
+            throw new TypeError('To must be a number')
+        }
+        if(newValue< this.from){
+            throw new RangeError('To must be a not < value1 ')
+        }
+        this._to = newValue
+    }
+
+    get to(){
+        return this._to
+    }
+
+    getRange(){
+        let arr = [];
+        for(let i = this.from;i<=this.to;i++){
+            arr.push(i)
+        }
+        
+        return arr 
+    }
+}
+
+const range = new RangeValidator(1,100)
